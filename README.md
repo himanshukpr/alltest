cdb80d45abe4a87fa50275a12cbcaf3acf1adc3c
Himanshu <himanshukpr12@gmail.com>
2024-08-02 13:03:09 +0530

    this is a message

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-this is somethign fucked up
+// webscraping --- to extract any content and store it.
+// setinterval
+// settimeout
+
+a = setInterval(()=>{
+    let date = new Date()
+    document.body.innerHTML = date.toLocaleTimeString();
+},(1000));
+
+// setTimeout(() => {
+//     console.log('done');
+// }, (2000));
+
+
+-callbacks-
+callbacks functions are those functions which accepts another function as a arguement
+function fun1(callback){};
+
+
+error handling
+error can be due to a wrong input
+try---
+it let us test the code of error
+catch ---
+to catch the error and handle
+throw---
+it throw the custom error
+finally---
+to execute the code even after getting any error in our code
+
+function fun1(a){
+    try{
+        if(isNaN(a)) throw "number only";
+        console.log(a)
+    }
+    catch(err){
+        console.log("must enter value...")
+    }
+    finally{
+        console.log("thanks for using")
+    }
+}
+fun1("uj");
+
+Promises
+these are the adbance version of error handling
+either resolve or reject
--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-let str = "hello this is a string" 
+// let str = "hello this is a string" 
 
-// split method is used to split a string to array..
-let a = str.split(" ");
+// // split method is used to split a string to array..
+// let a = str.split(" ");
 
-for(let i in a){
-    console.log(a[i]); 
-}
+// for(let i in a){
+//     console.log(a[i]); 
+// }
 
-// webscraping --- to extract any content and store it.
-// setinterval
-// settimeout
+// // webscraping --- to extract any content and store it.
+// // setinterval
+// // settimeout
 
+// a = setInterval(()=>{
+//     let date = new Date()
+//     document.body.innerHTML = date.toLocaleTimeString();
+// },(1000));
 
-a = setInterval(()=>{
-    let date = new Date()
-    document.body.innerHTML = date.toLocaleTimeString();
-},(1000));
+// // setTimeout(() => {
+// //     console.log('done');
+// // }, (2000));
 
-// setTimeout(() => {
-//     console.log('done');
-// }, (2000));
+// setTimeout(()=>{
+//     console.log("callbacks")
+// },2000)
 
+////////////// error handling
+// function fun1(a){
+//     try{
+//         if(isNaN(a)) throw "number only";
+//         console.log(a)
+//     }
+//     catch(err){
+//         console.log("must enter value...")
+//     }
+//     finally{
+//         console.log("thanks for using")
+//     }
+// }
+// fun1("uj");
 
-array.forEach(element => {
-    
-});
+///////////// promise
+
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        let date = new Date();
+        resolve(date.toLocaleTimeString());
+    }, 2000)
+})
